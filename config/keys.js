cf89a47e1218ade8efae89de6724dcf05d5aacfc
Pradeep Kumar <pradeepkumar@Pradeeps-MacBook-Air.local>
2018-11-19 13:20:55 +0530

    jwt token

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
 module.exports = {
   mongoURI:
-    "mongodb://prad1386:Passw0rd@ds123796.mlab.com:23796/devconnectorprad"
+    "mongodb://prad1386:Passw0rd@ds123796.mlab.com:23796/devconnectorprad",
+  secretOrKey: "secret"
 };
