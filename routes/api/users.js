158bd515126ef22bbe7d855637cb8f7980ecf8c0
Pradeep Kumar <pradeepkumar@Pradeeps-MacBook-Air.local>
2018-11-28 22:14:39 +0530

    react login and register componenets with errors

--- routes/api/users.js
+++ routes/api/users.js
@@ -13,7 +13,7 @@ const validateLoginInput = require("../../validation/login");
 //Load User model
 const User = require("../../models/User");
 
-// @route GET api/users/register
+// @route POST api/users/register
 // @desc Register user
 // @access Public
 
--- validation/register.js
+++ validation/register.js
@@ -14,11 +14,11 @@ module.exports = function validateRegisterInput(data) {
   }
 
   if (Validator.isEmpty(data.name)) {
-    errors.name = "Name fiels is required";
+    errors.name = "Name field is required";
   }
 
   if (Validator.isEmpty(data.email)) {
-    errors.email = "email fiels is required";
+    errors.email = "email field is required";
   }
 
   if (!Validator.isEmail(data.email)) {
